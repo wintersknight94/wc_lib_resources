1f96bc583c944a4db7fb4ebc9be8f64b73bace07
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-09-22 17:14:56 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# wc_lib_resources
-A resource library for NodeCore mods.
+# NodeCore Mod Resource Library
+A collection of resources I keep finding myself using in mods,
+so I've compiled them here so you don't have to have multiple 
+copies of the same redundant data if using multiple NC mods.
---
+++ init.lua
@@ -0,0 +1,8 @@
+-- LUALOCALS < ---------------------------------------------------------
+local include, nodecore
+    = include, nodecore
+-- LUALOCALS > ---------------------------------------------------------
+local modname = minetest.get_current_modname()
+-- ================================================================== --
+
+--include("")
